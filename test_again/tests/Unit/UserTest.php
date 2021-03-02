079c6ee66f76633b2f5ed9fa66656d2447f11559
cjl72 <cjl72@njit.edu>
2021-03-02 15:27:56 -0500

    Initialize repository

---
+++ test_again/tests/Unit/UserTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $users = User::factory()->count(3)->create();
+        $user = User::find(1);
+        $user->name = "CONNOR";
+        $user->save();
+    }
+}
